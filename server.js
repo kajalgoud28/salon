bc24829a70ec3fdb63a9c88542e058a656ae6526
kajalgoud28 <kajalgoud728@gmail.com>
2025-01-15 07:16:20 +0000

    Fix server binding and port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+
+// Use process.env.PORT if available, otherwise default to 3000
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+    res.send('Hello, world!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
